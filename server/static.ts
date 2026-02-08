ffce96e4b9d02be91de4db6c465749ce6eaf04c1
adityadeshpande41 <adityadeshpande41@gmail.com>
2026-02-07 20:51:59 -0500

    Fix Express 5 wildcard route syntax - use app.get instead of app.use

--- server/static.ts
+++ server/static.ts
@@ -18,7 +18,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.get("*", (_req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
